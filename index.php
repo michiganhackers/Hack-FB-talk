db980ce80ec2ba627e42284323db2e622b2cfefb
Gaurav Kulkarni <kgaurav@umich.edu>
2012-03-23 21:47:59 -0300

    Everything needs to have relative urls

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(!$user)
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" type="text/css" href="/assets/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css" />
 </head>
 <body>
   <div class="container" style="margin-top: 20px">
